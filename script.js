8c81ae168d844aef1137be086f10bfed5aa17fdb
TitanVHs <titanbog7@gmail.com>
2023-11-10 17:17:50 -0300

    update

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ let listaDeTarefas = document.querySelector('.listaDeTarefas');
 let input = document.querySelector('.input');
 let submitButton = document.querySelector('.submitButton');
 let deleteButton = document.querySelector('.deleteButton');
-
-let items = [];
+let clearButton = document.querySelector('.clearButton');
 
 function submitItem() {
     if(input.value) {
         let span = document.createElement('span');
-        span.innerHTML = `${input.value} <i class="ri-delete-bin-7-line deleteIcon"></i>`;
+        span.innerHTML = `${input.value} <i class="ri-delete-bin-7-line deleteButton"></i>`;
 
-        items = span;
-        listaDeTarefas.prepend(items);
+        listaDeTarefas.prepend(span);
     
         input.value = '';
+
+        clearButton.style.display = 'block';
+        clearButton.classList.remove('disabled');
     };
 };
 
+clearButton.addEventListener('click', function() {
+    listaDeTarefas.innerHTML = '';
+
+    clearButton.classList.add('disabled');
+});
+  
 input.addEventListener("keydown", function(e) {
     if (e.keyCode === 13) {
       submitItem();
