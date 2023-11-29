c8486ffbf7d17751e222828bcff2d0df69d5081f
Octávio Augusto <titanbog7@gmail.com>
2023-11-29 17:09:49 -0300

    to-do list update

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 let listaDeTarefas = document.querySelector('.listaDeTarefas');
-let input = document.querySelector('.input');
 let submitButton = document.querySelector('.submitButton');
-let deleteButton = document.querySelector('.deleteButton');
+let input = document.querySelector('.input');
 let clearButton = document.querySelector('.clearButton');
 
-function submitItem() {
-    if(input.value) {
-        let span = document.createElement('span');
-        span.innerHTML = `${input.value} <i class="ri-delete-bin-7-line deleteButton"></i>`;
+submitButton.addEventListener('click', function() {
+    //Função que adiciona uma tarefa.
+    const inputValue = input.value;
+    const li = document.createElement('li');
+    li.textContent = inputValue;
 
-        listaDeTarefas.prepend(span);
-    
-        input.value = '';
-
-        clearButton.style.display = 'block';
+    if(inputValue.trim() !== '') {
+        listaDeTarefas.append(li);
         clearButton.classList.remove('disabled');
-    };
-};
+    } else {
+        alert('Insira uma tarefa.');
+    }
+
+    input.value = '';
+
+    //Função que deleta uma tarefa.
+    const span = document.createElement('span');
+    span.className = 'close';
+    span.innerHTML = '<i class="ri-close-line"></i>';
+    li.appendChild(span);
+    
+    const close = document.getElementsByClassName('close');
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        const div = this.closest('li');
+        div.remove();
+        checkTaskList();
+      }
+    }
+});
 
 clearButton.addEventListener('click', function() {
     listaDeTarefas.innerHTML = '';
-
     clearButton.classList.add('disabled');
 });
-  
-input.addEventListener("keydown", function(e) {
-    if (e.keyCode === 13) {
-      submitItem();
+
+function checkTaskList() {
+    if (listaDeTarefas.children.length === 0) {
+        clearButton.classList.add('disabled');
     }
-});
+}
